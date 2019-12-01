c63a8128fb7637976dbf5f67348918a967d4130c
levicosta201 <levi.costa1@gmail.com>
2019-12-01 12:54:20 -0300

    fix routes

--- Core/Kernel/Request.php
+++ Core/Kernel/Request.php
@@ -1,50 +1,51 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace Core\Kernel;
 
 use Core\Kernel\IRequest;
 
-Class Request implements IRequest {
-
-	function __construct()
+Class Request implements IRequest
+{
+    
+    function __construct()
     {
-    	$this->bootstrapSelf();
+        $this->bootstrapSelf();
     }
     private function bootstrapSelf()
-	{
-    	foreach($_SERVER as $key => $value) {
-      		$this->{$this->toCamelCase($key)} = $value;
-		}
-  	}
-
-  	private function toCamelCase($string)
-  	{
-    	$result = strtolower($string);
+    {
+        foreach ($_SERVER as $key => $value) {
+            $this->{$this->toCamelCase($key)} = $value;
+        }
+    }
+    
+    private function toCamelCase($string)
+    {
+        $result = strtolower($string);
         
-    	preg_match_all('/_[a-z]/', $result, $matches);
-
-    	foreach($matches[0] as $match) {
-        	$c = str_replace('_', '', strtoupper($match));
-        	$result = str_replace($match, $c, $result);
-    	}
-    	return $result;
-  	}
-
-  	public function getBody()
-  	{
-    	if($this->requestMethod === "GET") {
-      		return;
-    	}
-
-    	if ($this->requestMethod == "POST") {
-      		
-      		$body = array();
-
-      		foreach($_POST as $key => $value) {
-        		$body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-      		}
-      		return $body;
-    	}
-  	}
+        preg_match_all('/_[a-z]/', $result, $matches);
+        
+        foreach ($matches[0] as $match) {
+            $c      = str_replace('_', '', strtoupper($match));
+            $result = str_replace($match, $c, $result);
+        }
+        return $result;
+    }
+    
+    public function getBody()
+    {
+        if ($this->requestMethod === "GET") {
+            return;
+        }
+        
+        if ($this->requestMethod == "POST") {
+            
+            $body = array();
+            
+            foreach ($_POST as $key => $value) {
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+            return $body;
+        }
+    }
 }
