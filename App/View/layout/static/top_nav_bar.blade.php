4c2404a33f8190473d375f673f9423ca1a15eec5
levicosta201 <levi.costa1@gmail.com>
2019-12-02 01:51:32 -0300

    remove comment

--- App/View/layout/static/top_nav_bar.blade.php
+++ App/View/layout/static/top_nav_bar.blade.php
@@ -11,15 +11,12 @@
             <a class="dropdown-item" href="{{ $user_data->html_url }}" target="_blank">
                 <i class="fab fa-github-square fa-sm fa-fw mr-2 text-gray-400"></i> Github Profile
             </a>
-            <a class="dropdown-item" href="#">
+            <!-- <a class="dropdown-item" href="#">
                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile
             </a>
             <a class="dropdown-item" href="#">
                 <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> Settings
-            </a>
-            <a class="dropdown-item" href="#">
-                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity Log
-            </a>
+            </a> -->
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout
