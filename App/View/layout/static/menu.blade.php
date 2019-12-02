94d06e41aa609117c65626b872575ac9ba140c9c
levicosta201 <levi.costa1@gmail.com>
2019-12-02 01:49:11 -0300

    fix url to redirect dashboard in menu

--- App/View/layout/static/menu.blade.php
+++ App/View/layout/static/menu.blade.php
@@ -1,6 +1,6 @@
 <!-- Nav Item - Dashboard -->
 <li class="nav-item">
-    <a class="nav-link" href="index.html">
+    <a class="nav-link" href="{{ url('/auth/dashboard/') }}">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
 </li>
