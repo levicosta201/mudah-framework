c4da8a22f050b3f367b7f9dd74afefe9732e5350
levicosta201 <levi.costa1@gmail.com>
2019-12-02 01:46:56 -0300

    add token, state and code in global variable property

--- App/Controller/DashboardController.php
+++ App/Controller/DashboardController.php
@@ -32,27 +32,73 @@ class DashboardController extends Controller
      */
 	private $response;
 
+	private $access_token;
+
 	private $git_hub_controller;
+
+	private $state;
+
+	private $code;
 
 	public function __construct(ResponseInterface $response)
 	{
 		parent::__construct();
 		$this->response = $response;
 		$this->git_hub_controller = new GitHubController;
+		$this->access_token = getSession('access_token');
+		$this->state = getSession('state');
+		$this->code = getSession('code');	
 	}
 
 	public function index()
 	{
-		$state = getSession('state');
-		$code = getSession('code');
-		$access_token = getSession('access_token');
+		$user_data = $this->git_hub_controller->getUserData($this->access_token);
+		$repos = $this->git_hub_controller->getUserRepos($this->access_token);
 
-		$user_data = $this->git_hub_controller->getUserData($access_token);
-		$repos = $this->git_hub_controller->getUserRepos($access_token);
-
-		return view('dashboard.index', [
+		return view('auth.dashboard.index', [
 			'user_data' => $user_data,
 			'repos' => $repos,
+		]);
+	}
+
+	public function search($request)
+	{
+		$request = (object) $request->getQueryParams();
+		$search_term = $request->q;
+		$search_result = new \stdClass;
+		$user_data = $this->git_hub_controller->getUserData($this->access_token);
+
+		for($i = 0; $i < 5; $i++) {
+			$language = '';
+
+			if($i == 0)
+				$language = 'php';
+			elseif($i == 1)
+				$language = 'java';
+			elseif($i == 2)
+				$language = 'javascript';
+			elseif($i == 3)
+				$language = 'C';
+			elseif($i == 4)
+				$language = 'c++';
+
+			$search_result->{$i} = $this->git_hub_controller->searchRepo('https://api.github.com/search/repositories', $this->access_token, $search_term, $language);
+			$search_result->{$i}->language = $language;
+		}
+
+		//first convert object result form search to array
+		$search_result_to_array = (array) $search_result;
+
+		//after converted suffle values into array to best experience
+		shuffle($search_result_to_array);
+
+		//and convert array to object again
+		$search_result_to_object = (object) $search_result_to_array;
+
+		return view('auth.search.result', [
+			'search_result' => $search_result_to_object,
+			'user_data' => $user_data,
+			'search_term' => $search_term,
 		]);
 	}
 }
