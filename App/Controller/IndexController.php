3ce5ed2439842f73e5a7c05abe3147a2009802b7
Levi <levi.costa1@gmail.com>
2019-12-11 17:19:25 -0300

    unused files and new fixes to request

--- App/Controller/IndexController.php
+++ App/Controller/IndexController.php
@@ -16,7 +16,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface;
-use App\Model\LogModel as LogModel;
 
 /**
  * Class LogsController.
@@ -41,35 +40,63 @@ class IndexController extends Controller
      */
 	private $session_id;
 
-	public function __construct(ResponseInterface $response)
-	{
-		parent::__construct();
-		$this->response = $response;
-	}
-
 	public function index()
 	{		
-
-		if(!$this->checkSessionId())
-			$this->validateSession();
-		dd($this->getProductData(40));
+		$this->getSessionId();
+		//json_decode($this->getProductData(35))->response
 		return $this->blade->make('index')->with([
 			'session_id' => $this->getSessionId(),
 		]);
 	}
 
-	private function autoLogin() : string
+	private function getSessionId()
 	{
 		$xml_raw = '<?xml version="1.0" encoding="ISO-8859-1" ?><serviceRequest><requestBody><NOMUSU>teste</NOMUSU><INTERNO>innovation</INTERNO></requestBody></serviceRequest>';
 		
-		return $this->requestData('http://innovation.fmcdatacom.com.br:8332/mge/service.sbr?serviceName=MobileLoginSP.login', $xml_raw);
+		$response = simplexml_load_string($this->requestData('http://innovation.fmcdatacom.com.br:8332/mge/service.sbr?serviceName=MobileLoginSP.login', $xml_raw));
+		$this->saveSessionId($response->responseBody->jsessionid);
+		return $response->responseBody->jsessionid;
 	}
 
-	private function getProductData(int $product_code) : string
+	public function getProductData(string $product_code = null)
 	{
-		$xml_raw = '{"requestBody":{"sql":"Select * From [sankhya].[VW_DadosProduto] Where codprod = 0060"}}';
-		
-		return $this->requestData('http://innovation.fmcdatacom.com.br:8332/mge/service.sbr?serviceName=DbExplorerSP.executeQuery&mgeSession='. $this->getSessionId(), addslashes($xml_raw));
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_PORT => "8332",
+		  CURLOPT_URL => "http://innovation.fmcdatacom.com.br:8332/mge/service.sbr?serviceName=DbExplorerSP.executeQuery&mgeSession=KfnUMQPfEDxw6bwK948DILDVVu9sJhfr-Q2Jb2yT",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => "{\"serviceName\":\"DbExplorerSP.executeQuery\",\"requestBody\":{\"sql\":\"SELECT * FROM [sankhya].[VW_DadosProduto] WHERE codprod = ".$product_code." \"}}",
+		  CURLOPT_HTTPHEADER => array(
+		    "Accept: */*",
+		    "Accept-Encoding: gzip, deflate",
+		    "Cache-Control: no-cache",
+		    "Connection: keep-alive",
+		    "Content-Length: 131",
+		    "Content-Type: application/html+xml",
+		    "Cookie: JSESSIONID=KfnUMQPfEDxw6bwK948DILDVVu9sJhfr-Q2Jb2yT.snk-app-treina02",
+		    "Host: innovation.fmcdatacom.com.br:8332",
+		    "Postman-Token: 1cdc67e9-de7f-480f-80b7-70f24ff73d5f,f6a8a0da-d7c7-478b-be48-e73964da0ec5",
+		    "User-Agent: PostmanRuntime/7.20.1",
+		    "cache-control: no-cache"
+		  ),
+		));
+
+		$response = json_decode(utf8_encode(curl_exec($curl)));
+
+		$err = curl_error($curl);
+		if ($err) 
+		    return json_encode([
+		        'error' => $err,
+		  	]);
+
+		return json_encode([
+			'response' => $response->responseBody,
+		]);
 	}
 
 	private function requestData(string $url, string $raw) : string
@@ -77,55 +104,49 @@ class IndexController extends Controller
 		$curl = curl_init();
 
 		curl_setopt_array($curl, array(
-			CURLOPT_PORT => "8332",
-		    CURLOPT_URL => $url,
-		    CURLOPT_RETURNTRANSFER => true,
-		    CURLOPT_ENCODING => "",
-		    CURLOPT_MAXREDIRS => 10,
-		    CURLOPT_TIMEOUT => 30,
-		    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		    CURLOPT_CUSTOMREQUEST => "POST",
-		    CURLOPT_POSTFIELDS => $raw,
-		    CURLOPT_HTTPHEADER => array(
-		        "cache-control: no-cache",
-			    "content-type: text/xml",
-			    "postman-token: 1c4a383b-51d9-5e08-b457-7184be176fec"
-		    ),
+		  CURLOPT_PORT => "8332",
+		  CURLOPT_URL => $url,
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 60,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $raw,
+		  CURLOPT_HTTPHEADER => array(
+		    "Accept: */*",
+		    "Accept-Encoding: gzip, deflate",
+		    "Cache-Control: no-cache",
+		    "Connection: keep-alive",
+		    "Content-Length: 155",
+		    "Content-Type: application/xml",
+		    "Cookie:".$this->getSessionIdFromSession(),
+		    "Host: innovation.fmcdatacom.com.br:8332",
+		    "Postman-Token: a25f84bb-01d4-4b21-b2ac-89e7a9f6194b,27b4365c-543e-44d2-83bd-8dba3f31052c",
+		    "User-Agent: PostmanRuntime/7.20.1",
+		    "cache-control: no-cache"
+		  ),
 		));
 
 		$response = curl_exec($curl);
+
 		$err = curl_error($curl);
-		dd([
-			$response,
-			$err,
-		]);
+
 		curl_close($curl);
 
-		if ($err)
-		  return "cURL Error #:" . $err;
-	  return $response;
-
+		if ($err) 
+		  return "Error #:" . $err;
+		
+		return $response;
 	}
 
-	private function checkSessionId() : bool 
-	{
-		$get_session_id = $this->getSessionId();
-
-		if($get_session_id != null or $get_session_id != '')
-			return true;
-		return false;
+	private function saveSessionId($session_id) : void
+	{	
+		saveSession('session_id', (string) $session_id);
 	}
 
-	private function validateSession() : void
+	private function getSessionIdFromSession() : string
 	{
-		$auto_login = stringToXml($this->autoLogin());
-		$session_id = (string) $auto_login->responseBody->jsessionid;				
-		saveSession('session_id', $session_id);
-		$this->session_id = $session_id;
-	}
-
-	private function getSessionId() : string
-	{
-		return getSession('session_id') ? getSession('session_id') : $this->session_ids;
+		return getSession('session_id') ?? '';
 	}
 }
